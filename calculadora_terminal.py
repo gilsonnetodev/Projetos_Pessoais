b423460ed11dc9550a31703624312898a6cdbde9
Gilson Neto <gilsongamer4321@gmail.com>
2024-01-10 08:21:37 -0300

    Interface gráfica Criada

--- calculadora_terminal.py
+++ calculadora_terminal.py
@@ -1,40 +1,58 @@
-def linha():
-    print('-='*11)
+from tkinter import *
 
+def somar():
+    n1 = float(text_num1.get())
+    n2 = float(text_num2.get())
+    resultado = Label(fimframe, font= 'Arial 13',text= f'A Soma é {n1 + n2}')
+    resultado.pack(padx= 10, pady= 10)
 
-linha()
-print('     CALCULADORA')
-linha()
-while True:
-    linha()
-    print('''[1] SOMAR
-[2] SUBTRAIR
-[3] MULTIPLICAR
-[4] DIVIDIR
-[5] SAIR''')
-    linha()
-    try:
-        resposta = int(input('Selecione a Operação: '))
-        if resposta > 5 or resposta <= 0:
-            print('\033[31mOpção invalida! Tente novamente\033[m')
-    except:
-        print('\033[31mOpção invalida! Tente novamente\033[m')
-    if resposta == 1:
-        a = float(input('Primeiro valor: '))
-        b = float(input('Sugundo valor: '))
-        print(f'\033[33m{a} + {b} = {a + b}\033[m')
-    if resposta == 2:
-        a = float(input('Primeiro valor: '))
-        b = float(input('Sugundo valor: '))
-        print(f'\033[33m{a} - {b} = {a - b}\033[m')
-    if resposta == 3:
-        a = float(input('Primeiro valor: '))
-        b = float(input('Sugundo valor: '))
-        print(f'\033[33m{a} * {b} = {a * b}\033[m')
-    if resposta == 4:
-        a = float(input('Primeiro valor: '))
-        b = float(input('Sugundo valor: '))
-        print(f'\033[33m{a} / {b} = {a / b}\033[m')
-    if resposta == 5:
-        print('Programa encerrado, Volte sempre!')
-        break
+def subtrair():
+    n1 = float(text_num1.get())
+    n2 = float(text_num2.get())
+    resultado = Label(fimframe, font= 'Arial 13',text= f'A Subtração é {n1 - n2}')
+    resultado.pack(padx= 10, pady= 10)
+
+def multiplicar():
+    n1 = float(text_num1.get())
+    n2 = float(text_num2.get())
+    resultado = Label(fimframe, font= 'Arial 13',text= f'A Multiplicação é {n1 * n2}')
+    resultado.pack(padx= 10, pady= 10)
+
+def dividir():
+    n1 = float(text_num1.get())
+    n2 = float(text_num2.get())
+    resultado = Label(fimframe,font= 'Arial 13',text= f'A Divisão é {n1 / n2}')
+    resultado.pack(padx= 10, pady= 10)
+
+janela = Tk()
+janela.title('APP')
+frame1 = Frame(janela)
+frame2 = Frame(janela)
+pc_frame = Frame(janela)
+fimframe = Frame(janela)
+
+num1 = Label(pc_frame, text= 'Primeiro Número', font= 'arial 13')
+text_num1 = Entry(pc_frame, font= 'arial 13')
+num2 = Label(pc_frame, text= 'Segundo Número', font= 'arial 13')
+text_num2 = Entry(pc_frame, font= 'arial 13')
+
+bnt_soma = Button(frame1, text='Somar', font='arial 13', command= somar)
+bnt_subtrair = Button(frame1, text='Subtrair', font='arial 13', command= subtrair)
+bnt_multi = Button(frame2, text='Multiplicar', font='arial 13', command= multiplicar)
+bnt_dividir = Button(frame2, text='Dividir', font='arial 13', command= dividir)
+
+num1.pack(padx= 10, pady= 10)
+text_num1.pack(padx= 10, pady= 10)
+num2.pack(padx= 10, pady= 10)
+text_num2.pack(padx= 10, pady= 10)
+
+bnt_soma.pack(padx= 10, pady= 10)
+bnt_subtrair.pack(padx= 10, pady= 10)
+bnt_multi.pack(padx= 10, pady= 10)
+bnt_dividir.pack(padx= 10, pady= 10)
+pc_frame.grid()
+frame1.grid(row=1, sticky=W)
+frame2.grid(row=1, sticky=E)
+fimframe.grid()
+
+janela.mainloop()
